d18733f16a172df3eac8f76e5ed0914f7e835be9
Sidath <sidathm@99x.io>
2023-07-20 22:12:07 +0530

    Add bootstrap

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,55 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavigationBar() {
+  return (
+    // <Navbar bg='dark' data-bs-theme='dark'>
+    //   <Container>
+    //     <Navbar.Brand href='#home'>Workflow Monitor</Navbar.Brand>
+    //     <Nav className='me-auto'>
+    //       <NavDropdown title='Workflows' id='navbarScrollingDropdown'>
+    //         <NavDropdown.Item href='#action3'>In Progress</NavDropdown.Item>
+    //         <NavDropdown.Item href='#action4'>Completed</NavDropdown.Item>
+    //         <NavDropdown.Item href='#action5'>Failed</NavDropdown.Item>
+    //       </NavDropdown>
+    //       <Nav.Link href='#features'>FAQ</Nav.Link>
+    //     </Nav>
+    //   </Container>
+    // </Navbar>
+    <Navbar
+      bg='dark'
+      data-bs-theme='dark'
+      collapseOnSelect
+      expand='lg'
+      className='bg-body-tertiary'
+    >
+      <Container>
+        <Navbar.Brand href='#home'>Workflow Monitor</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto'>
+            <NavDropdown title='Workflows' id='collasible-nav-dropdown'>
+              <NavDropdown.Item href='#action/3.1'>In Progress</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.2'>
+                Completed
+              </NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.4'>
+                Failed
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href='#pricing'>FAQ</Nav.Link>
+            <Nav.Link eventKey={2} href='#memes'>
+              Profile
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
