bddaf5767f44a3df7e4b9ab349010551849d8d05
Sidath <sidathm@99x.io>
2023-07-20 23:19:49 +0530

    Add error page

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import NavigationBar from './components/Navbar';
 import HomePage from './pages/Home';
 import InProgressWorkflowsPage from './pages/Workflows/InProgress';
 import CompletedWorkflowsPage from './pages/Workflows/Completed';
+import ErrorPage from './pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: '/workflows/inprogress',
---
+++ src/pages/ErrorPage/index.tsx
@@ -0,0 +1,9 @@
+function ErrorPage() {
+  return (
+    <div>
+      <h1>Something went wrong!</h1>
+    </div>
+  );
+}
+
+export default ErrorPage;
