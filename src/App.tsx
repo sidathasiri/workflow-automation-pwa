b114f2d90a651c72fe5669ca243320eecd777673
Sidath <sidathm@99x.io>
2023-07-20 23:10:29 +0530

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import NavigationBar from './components/Navbar';
 import HomePage from './pages/Home';
+import InProgressWorkflowsPage from './pages/Workflows/InProgress';
+import CompletedWorkflowsPage from './pages/Workflows/Completed';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/workflows/inprogress',
+    element: <InProgressWorkflowsPage />,
+  },
+  {
+    path: '/workflows/completed',
+    element: <CompletedWorkflowsPage />,
+  },
+]);
 
 function App() {
   return (
     <div className='App'>
       <NavigationBar />
-      <HomePage />
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -13,20 +13,22 @@ function NavigationBar() {
       className='bg-body-tertiary'
     >
       <Container>
-        <Navbar.Brand href='#home'>Workflow Monitor</Navbar.Brand>
+        <Navbar.Brand href='/'>Workflow Monitor</Navbar.Brand>
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
         <Navbar.Collapse id='responsive-navbar-nav'>
           <Nav className='me-auto'>
             <NavDropdown title='Workflows' id='collasible-nav-dropdown'>
-              <NavDropdown.Item href='#action/3.1'>
+              <NavDropdown.Item href='/workflows/inprogress'>
                 In Progress
               </NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.2'>Completed</NavDropdown.Item>
+              <NavDropdown.Item href='/workflows/completed'>
+                Completed
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href='#pricing'>FAQ</Nav.Link>
-            <Nav.Link eventKey={2} href='#memes'>
+            <Nav.Link href='#faq'>FAQ</Nav.Link>
+            <Nav.Link eventKey={2} href='#profile'>
               Profile
             </Nav.Link>
           </Nav>
